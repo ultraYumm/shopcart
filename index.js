1f24950b58690faa5c2c0882af97d17d6bbc2678
John W Alden Jr <Alden-LeBlond27@Office-iMac.local>
2019-11-30 15:26:38 -0800

    Shopping cart

---
+++ index.js
@@ -0,0 +1,34 @@
+$(function() {
+  
+    $('#js-shopping-list-form').submit(event => {
+      // this stops the default form submission behavior
+      event.preventDefault();
+      const newItem = $(this).find('#shopping-list-entry');
+       newItem.val();
+      
+       $( 'ul').append(`<ul class="shopping-list">
+      <li>
+          <span class="shopping-item">${newItem.val()}</span>
+          <div class="shopping-item-controls">
+            <button class="shopping-item-toggle">
+              <span class="button-label">check</span>
+            </button>
+            <button class="shopping-item-delete">
+              <span class="button-label">delete</span>
+            </button>
+          </div>
+        </li>`)
+        });
+    
+    $(document).on('click', '.shopping-item-delete', function(event) {
+      $(this).closest("li").remove();
+        });
+    
+    $(document).on('click', '.shopping-item-toggle', function(event) {
+      $(this).closest("li",".shopping-item").toggleClass("shopping-item__checked");
+  
+        });
+  
+  
+  });  
+  
